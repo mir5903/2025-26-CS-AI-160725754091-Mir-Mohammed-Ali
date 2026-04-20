c9ce171e6fc19eca3829398169ec9176d0c76092
anastrow <72861950+mir5903@users.noreply.github.com>
2026-04-20 22:59:05 +0530

    Create selection sort non recursive.c

---
+++ selection sort non recursive.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, minIndex, temp;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input array
+    printf("Enter elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Selection Sort
+    for (i = 0; i < n - 1; i++) {
+        minIndex = i;
+
+        // Find minimum element
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // Swap
+        temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+
+    // Output sorted array
+    printf("Sorted array:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
